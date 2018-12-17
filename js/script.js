0ce36bfa8e7a9476d9a8e7c45e3af3ec97b3940b
Lokideos <archmage09@gmail.com>
2018-12-17 18:15:03 +0300

    Animate popups and check for errors for contact us modal

--- js/script.js
+++ js/script.js
@@ -6,14 +6,23 @@ var linkMap = document.querySelector(".map-widget-link");
 var popupMap = document.querySelector(".modal-map");
 var closeMapPopup = popupMap.querySelector(".modal-close");
 
+var contactUsSubmit = popupContactUs.querySelector(".submit")
+
+var contactForm = popupContactUs.querySelector(".contact-us-form")
+var contactName = popupContactUs.querySelector(".contact-us-name");
+var contactEmail = popupContactUs.querySelector(".contact-us-email");
+var contactText = popupContactUs.querySelector(".contact-us-text");
+
 linkContactUs.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupContactUs.classList.add("modal-show");
+  contactName.focus();
 });
 
 closeContactUsPopup.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupContactUs.classList.remove("modal-show");
+  popupContactUs.classList.remove("wrong-input");  
 });
 
 linkMap.addEventListener("click", function(evt) {
@@ -25,6 +34,14 @@ closeMapPopup.addEventListener("click", function(evt) {
   popupMap.classList.remove("modal-show");
 })
 
+contactForm.addEventListener("submit", function(evt) {
+  if (!contactName.value || !contactEmail.value  || !contactText.value) {
+    evt.preventDefault();
+    popupContactUs.classList.remove("wrong-input");
+    popupContactUs.classList.add("wrong-input");
+  }
+});
+
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
@@ -32,6 +49,7 @@ window.addEventListener("keydown", function(evt) {
     if (popupContactUs.classList.contains("modal-show") || popupMap.classList.contains("modal-show")) {
       popupContactUs.classList.remove("modal-show");
       popupMap.classList.remove("modal-show");
+      popupContactUs.classList.remove("wrong-input");  
     }
   }
 });
